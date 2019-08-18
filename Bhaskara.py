92996f67bbecab7d876798a1390b6b004f97cdcc
Lucazera04 <lucaslucasleandro@hotmail.com>
2019-08-18 00:29:48 -0300

    Versão: 1.0

---
+++ Bhaskara.py
@@ -0,0 +1,27 @@
+#Fórmula de Bhaskara - Equação do 2ºGrau (ax^2 + bx + c = 0)
+
+#Informando os valores da equação
+print("Bem-Vindo(a)!")
+print()
+a=float(input("Informe o valor de a: "))
+b=int(input("Informe o valor de b: "))
+c=float(input("Informe o valor de c: "))
+print()
+
+#Calculando Delta
+delta = (b^2) - (4*a*c)
+print("O delta é: ", delta)
+print()
+if delta < 0:
+    print("Raíz não pode ser negativo")
+else:
+#Resolvendo delta
+    bhaskara = (-b)
+    delta = delta ** (1/2)
+    X1 = (bhaskara + delta) / (2*a)
+    X2 = (bhaskara - delta) / (2*a)
+#Resolvendo Bhaskara
+    print("O X1 é: %.0f"%X1)
+    print("O X2 é: %.0f"%X2)
+    print()
+    print("S = {%.0f"%X1,"; %.0f"%X2,"}")
